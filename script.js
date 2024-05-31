857975343c1a67d5d299dbf8e4fce3f1559f10ee
mbitu <mbitumutonga@gmail.com>
2024-05-31 18:04:23 +0300

    added the different sections

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// script for the join now buttons
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            alert('Join Now button clicked!');
-        });
-    });
+            //alert('Join Now button clicked!');
+        });
+    });
+});
+// script for the scroll buttons
+document.getElementById('scrollToTop').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.getElementById('scrollToBottom').addEventListener('click', function() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 });
